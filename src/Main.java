bc34bbbf7e208d8f071619b1d0d7d47198b0ee94
Filya42 <dinil13@mail.ru>
2023-01-26 19:15:39 +0700

    Class B

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,43 @@
 import java.util.Scanner;
 
+class B
+{
+    void sp(){}
+}
+
+
 public class Main {
     public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+        int c = 0;
         Scanner sc = new Scanner(System.in);
-        int j = sc.nextInt();
-        while(true){
-            if ((j%2) == 0){
-                j = j / 2;
-            } else if ((j%1) == 0) {
-                j = j * 3 + 1;
+        //int j = sc.nextInt();
+        /*for(int j = 0; j < 100000; j++){
+            while(true) {
+                if(g == 3)
+
+                int g = 0;
+                if ((j % 2) == 0) {//если число четное разделить на 2
+                    j = j / 2;
+                } else if ((j % 1) == 0) {//если нечетное умножить на 3 прибавить 1
+                    j = j * 3 + 1;
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                if(g == 0)
+                    a = j;
+                if(g == 1)
+                    b = j;
+                if(g == 2)
+                    c == j;
+                g++
+                System.out.println(j);
+                if(a == 4);
             }
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println(j);
-        }
+        }*/
     }
 }
